7428c0ccbb5b99a6c8492449276918c005ede948
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2025-05-26 10:18:46 +0530

    changed config in auth controller

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -67,7 +67,7 @@ const loginUser = async (req, res) => {
       { expiresIn: "60m" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    res.cookie("token", token, { httpOnly: true, secure: true }).json({
       success: true,
       message: "Logged in Successfully",
       user: {
